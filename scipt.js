c4da8a83ea6ea4e4efd28003ab3880ac5cee16da
joemy14 <joemhay.avanzado@hcdc.edu.ph>
2024-12-12 23:55:20 -0600

    Delete scipt.js

--- scipt.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('show-password').addEventListener('change', function() {
-    const passwordInput = document.getElementById('password');
-    passwordInput.type = this.checked ? 'text' : 'password';
-  });
-});

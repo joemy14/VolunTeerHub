ba012594bb1f2b473badb90864035b5ee5374bc8
joemy14 <joemhay.avanzado@hcdc.edu.ph>
2024-12-12 23:54:19 -0600

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,8 +1,7 @@
-document.getElementById('show-password').addEventListener('change', function() {
-    var passwordField = document.getElementById('password');
-    if (this.checked) {
-        passwordField.type = 'text';  // Show the password
-    } else {
-        passwordField.type = 'password';  // Hide the password
-    }
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('show-password').addEventListener('change', function() {
+    const passwordInput = document.getElementById('password');
+    passwordInput.type = this.checked ? 'text' : 'password';
+  });
 });
